f7a47dc06db6c8a7224290ece62ecca8246e2fb0
ArseniyChepygin <arseniych735@gmail.com>
2025-04-26 19:09:47 +0200

    Started new project

--- src/App.js
+++ src/App.js
@@ -1,13 +1,51 @@
+import { createRoot } from "react-dom/client";
+import { Canvas, useFrame } from "@react-three/fiber";
+import { useRef } from "react";
+import { OrbitControls } from "@react-three/drei";
+
+import Cube from "./Components/Cube";
+import Face from "./Components/Face";
+
+import { useControls } from "leva";
 import React from "react";
 import './App.css';
-
-function App() {
+function RotatingBox() {
+  const meshRef = useRef();
+  // useFrame(() => {
+  //   if (meshRef.current) {
+  //     meshRef.current.rotation.x += 0.01
+  //     meshRef.current.rotation.y += 0.01
+  //   }
+  // });
   return (
-    <div className="App">
-     <h1>React-template</h1>
-     <p>Strater template for React-project</p>
-    </div>
+    <mesh ref={meshRef}>
+      <boxGeometry args={[3, 3, 3]}/>
+      <meshPhongMaterial color="yellow"/>
+    </mesh>
   );
 }
 
+function App() {
+  const colors = useControls("Hero colors", {
+    head: "#f00",
+    body: "#0f0",
+    hand: "#00f",
+    leg: "#0ff"
+  });
+
+  return (
+    <div id="canvas-container" className="App">
+      <Canvas camera={{ position: [5, 3, 5], fov: 50 }}>
+        {/* <RotatingBox /> */}
+        {/* <Cube colors={colors}/> */}
+        <Face colors={colors}/>
+        <ambientLight intensity={0.3} />
+        {/* <directionalLight position={[0, 0, 5]} color="red"/> */}
+        <directionalLight position={[0, 0, 5]} intensity={1}/>
+        <OrbitControls />
+      </Canvas>
+    </div>
+  );
+}
+createRoot(document.getElementById('root')).render(<App />)
 export default App;
